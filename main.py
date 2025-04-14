2eb398d80677b11134e72436662e74001393b7fa
Joseph Arias <arias.joseph.biot@gmail.com>
2025-04-14 23:03:12 +0200

    ajout de la gestion de phenotype, cohérence pour l'indetifier de vertex, ajout de la fonction mutation

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ with open("interaction_extraite_gavin2006.txt", 'r') as file:
         line = file.readline()
 
 
-def initialisation(graph: Graph, num_individuals: int) -> list[Graph]:
+def initialisation(graph: Graph, num_individuals: int) -> list:
     """
     Génère la population initiale P0 pour l'algorithme DECD.
-    :param graph: Le graphe d'intéraction
+    :param graph: le graphe d'intéraction
     :param num_individuals: nombre d'individus (partitions) à générer
     :return: liste d'individus (chaque individu = Graph)
     """
@@ -44,7 +44,37 @@ def initialisation(graph: Graph, num_individuals: int) -> list[Graph]:
     return population
 
 
-def clean_solution(graph: Graph, seuil: float) -> None:
+def mutation(population: list, f: float) -> list:
+    """
+
+    :param population:
+    :param f:
+    :return:
+    """
+    pop_mutante = []
+    j = 0
+    while len(population) != len(pop_mutante):
+        x1 = np.array(random.choice(population).to_genotype)
+        x2 = np.array(random.choice(population).to_genotype)
+        x3 = np.array(random.choice(population).to_genotype)
+        while x1 == x2 or x2 == x3 or x3 == x1:
+            x1 = np.array(random.choice(population).to_genotype)
+            x2 = np.array(random.choice(population).to_genotype)
+            x3 = np.array(random.choice(population).to_genotype)
+        v = (x1 + f * (x2 - x3)).tolist()
+        genotype_j = population[j].to_genotype
+        lower_bound = min(genotype_j)
+        upper_bound = max(genotype_j)
+        for i in range(len(v)):
+            if v[i] < lower_bound:
+                v[i] = (2 * lower_bound) - v[i]
+            elif v[i] > upper_bound:
+                v[i] = (2 * upper_bound) - v[i]
+        j += 1
+        pop_mutante.append(v)
+    return pop_mutante
+
+def clean_solution(graph: Graph, seuil: int) -> None:
     """
     Fonction de nettoyage basée sur la variance communautaire CV(i).
     :param graph:
@@ -70,14 +100,14 @@ def crossover(x: Graph, v: Graph, CR: float) -> Graph:
     """
     Recombine deux individus xi (cible) et vi (mutant) selon la stratégie DECD.
 
-    :param x: Solution cible
+    :param x: solution cible
     :param v: solution mutante
     :param CR: float – taux de recombinaison (probabilité de changer de communauté)
     :return: Graph() : nouvel individu u
     """
     u = copy(x)
     sommets = x.get_vertices()
-    j_rand = random.randint(0, len(sommets) - 1)
+    j_rand = random.randint(0, len(sommets)-1)
     j = 0
     sommets_u = u.get_vertices()
     sommets_v = v.get_vertices()
@@ -92,41 +122,44 @@ def crossover(x: Graph, v: Graph, CR: float) -> Graph:
     return u
 
 
-def DECD():
-    """
-     Entrée : NPi : le nombre d’individus, F : facteur d’échelle pour
-     rand/1, CR : la probabilité de croisement pour le
-     croisement binomiale de solution, η : le seuil pour le
-     nettoyage, NB : le nombre d’itérations
-    """
-    NP = 200
-    F = 0.9
-    CR = 0.3
-    n = 0.35
-    NB = 200
-    t = 0
-    P = initialisation(graphe, NP)
-    Qx, Qu = [], []
-    for i in range(NP):
-        Qx[i] = P[i].modularity
-    while t < NB:
-        V = mutation(P, F)
-        for i in range(len(V)):
-            clean_solution(V[i], n)
-            crossover(V[i], P[i], CR)
-            clean_solution(V[i], n)
-        for i in range(NP):
-            Qu[i] = V[i].modularity
-            if Qx[i] <= Qu[i]:
-                P[i] = V[i]
-        t += 1
-    Xbest = P[0]
-    for i in range(1, NP):
-        if Xbest.modularity < P[i].modularity:
-            Xbest = P[i]
+NP = 200
+t = 0
+QX = []
+QU = []
+F = 0.9
+CR = 0.3
+n = 0.35
+NB = 200
+P = initialisation(graphe, NP)
+"""for i in range(1, NP):
+    QX[i] = modularity(P[i])
+while t < NB:
+    V = mutation(P, F)
+    V = nettoyage(V, n)
+    U = recombine(P)
+    U = nettoyage(U, n)
+    for i in range(1,NP):
+        QU[i] = modularity(U[i])
+        if QX[i] > QU[i]:
+            P[i] = xi
+        else:
+            P[i] = ui
+    t += 1
+Xbest = P[1]
+for i in range(2, NP):
+    if modularite(Xbest) < modularite(P[i]):
+        Xbest = P[i]
+"""
+
+"""
+Entrée : NPi : le nombre d’individus, F : facteur d’échelle pour
+rand/1, CR : la probabilité de croisement pour le
+croisement binomiale de solution, η : le seuil pour le
+nettoyage, NB : le nombre d’itérations
+"""
 
 
 part = initialisation(graphe, 200)
 """clean = clean_solution(graphe, part[0], 0.35)"""
 clean_solution(graphe, 50)
-# recombinaison = crossover(graphe, graphe, 0.3)
+#recombinaison = crossover(graphe, graphe, 0.3)
