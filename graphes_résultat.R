7674485432ce00ef53dfe528a79bc5380587c6c3
uvsq22200379 <114011966+uvsq22200379@users.noreply.github.com>
2025-04-25 16:11:11 +0200

    modif legere du fichier pour cr

--- graphes_résultat.R
+++ graphes_résultat.R
@@ -1,15 +1,19 @@
+# ÉVOLUTION MODULARITÉ AVEC PROBA À 0.1
 # plot pour voir évolution de la modularite avec une courbe par lignes executées
 df <- read.delim("evolution_modularite.txt", header = FALSE, sep = "\t", skip = 1, fill = TRUE)
 x <- 1:ncol(df)
-
+png("evolution_modularite_P0.1.png", width = 800, height = 600, bg = "white")
 plot(x, xlim = c(1, ncol(df)), ylim = c(0.32,0.57),
-     xlab = "Générations", ylab = "Valeurs de la modularité", main = "Évolution de la modularité")
+     xlab = "Génération", ylab = "Valeurs de la modularité",cex.lab=1.4)
 
 colors <- rainbow(nrow(df))
 for (i in 1:nrow(df)) {
   y <- as.numeric(df[i, ])
   lines(x, y, col = colors[i])
 }
+
+title(main = "Évolution de la modularité", cex.main = 2, font.main= 2)
+dev.off()
 
 #boxplot pour voir la moyenne de la modularité par génération
 library(tidyverse)
@@ -19,7 +23,7 @@ df_clean <- na.omit(df)
 df_clean <- df_clean[, 1:200]
 colnames(df_clean) <- paste0("Gen", 1:ncol(df_clean))
 
-
+png("evolution_modularite_grilles_P0.1.png", width = 800, height = 600, bg = "white")
 df_long <- df_clean %>%
   pivot_longer(cols = everything(), names_to = "Generation", values_to = "Value") %>%
   mutate(
@@ -37,11 +41,15 @@ ggplot(df_long, aes(x = GenLabel, y = Value)) +
   facet_wrap(~FacetGroup, scales = "free_x", ncol = 5) +
   labs(x = "Génération", y = "Valeurs de la modularité", title = "Evolution de la modularité") +
   theme_minimal() +
-  theme(strip.background = element_rect(fill = "grey20"),
-        strip.text = element_text(color = "white"),
-        panel.background = element_rect(fill = "black",size = 1, linetype = "solid"),
-        plot.background = element_rect(fill = "black"),
-        plot.title = element_text(color = "white",hjust = 0.5, size=14, face="bold"),
-        axis.text = element_text(color = "white", size = 6),
-        axis.title = element_text(color = "white"),
-        panel.grid = element_line(color = "grey30"))
+  theme(
+  strip.background = element_rect(fill = "white"),
+  strip.text = element_text(color = "black"),
+  panel.background = element_rect(fill = "white", size = 1, linetype = "solid"),
+  plot.background = element_rect(fill = "white"),
+  plot.title = element_text(color = "black", hjust = 0.5, size = 14, face = "bold"),
+  axis.text = element_text(color = "black", size = 6),
+  axis.title = element_text(color = "black"),
+  panel.grid = element_line(color = "grey70")
+)
+
+dev.off()
